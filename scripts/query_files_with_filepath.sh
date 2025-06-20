1aaba22720e4d94f1ec8065d4124735929cbe688
Nanjiang Shu <nanjiang.shu@gmail.com>
2025-06-20 23:32:44 +0200

    update script

--- scripts/query_files_with_filepath.sh
+++ scripts/query_files_with_filepath.sh
@@ -1,11 +1,45 @@
-filepath=$1
+#!/bin/bash
+# This script queries the files table in the sda database for a specific filepath.
+
+substring_match=false
+
+usage="""
+Usage: $0 [OPTIONS] <filepath>
+Options:
+  -h, --help          Show this help message and exit
+  -s, --substring     Use substring match for filepath (default is exact match)
+"""
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -h|--help)
+            echo "$usage"
+            exit 0
+            ;;
+        -s|--substring)
+            substring_match=true
+            shift
+            ;;
+        *)
+            filepath=$1
+            shift
+            ;;
+    esac
+done
 
 if [ "$filepath" == "" ];then
+    echo "$usage"
     exit 1
 fi
 
+if [ "$substring_match" = true ]; then
+    filepath_condition="submission_file_path LIKE '%$filepath%'"
+else
+    filepath_condition="submission_file_path = '$filepath'"
+fi
+filepath=$(echo "$filepath" | xargs)
+
 kubectl -n sda-prod exec svc/postgres-cluster-ro -c postgres -- psql -U postgres  -d sda -c "
 SELECT * FROM sda.files
-WHERE submission_file_path = '$filepath'
+WHERE $filepath_condition 
 "
 
