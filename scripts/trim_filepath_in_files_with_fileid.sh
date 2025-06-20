b9e9303361e3a8731c0affbaef5333e957923524
Nanjiang Shu <nanjiang.shu@gmail.com>
2025-06-20 23:47:19 +0200

    use regexp_replace instead of trim to remove all whitespaces

--- scripts/trim_filepath_in_files_with_fileid.sh
+++ scripts/trim_filepath_in_files_with_fileid.sh
@@ -7,6 +7,9 @@ if [ "$file_id" == "" ];then
     exit 1
 fi
 
+# Trim trailing whitespace, including spaces, tabs and newlines, from the submission_file_path
 kubectl -n sda-prod exec svc/postgres-cluster-rw -c postgres -- psql -U postgres -d sda -c "
-UPDATE sda.files SET submission_file_path = TRIM(submission_file_path)
-WHERE id = '$file_id'" 
+UPDATE sda.files
+SET submission_file_path = REGEXP_REPLACE(submission_file_path, '\s+$', '')
+WHERE id = '$file_id'
+"
