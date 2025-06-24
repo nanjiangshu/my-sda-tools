3f724d788c64fc109f66d733741cd1fab7ae6223
Nanjiang Shu <nanjiang.shu@gmail.com>
2025-06-24 22:20:53 +0200

    update script

--- scripts/query_files_with_filepath.sh
+++ scripts/query_files_with_filepath.sh
@@ -2,12 +2,14 @@
 # This script queries the files table in the sda database for a specific filepath.
 
 substring_match=false
+no_header=false
 
 usage="""
 Usage: $0 [OPTIONS] <filepath>
 Options:
   -h, --help          Show this help message and exit
   -s, --substring     Use substring match for filepath (default is exact match)
+  -nh, --no-header    Do not include header in the output (default is to include header)
 """
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -19,6 +21,10 @@ while [[ $# -gt 0 ]]; do
             substring_match=true
             shift
             ;;
+        -nh|--no-header)
+            no_header=true
+            shift
+            ;;  
         *)
             filepath=$1
             shift
@@ -38,7 +44,16 @@ else
 fi
 filepath=$(echo "$filepath" | xargs)
 
-kubectl -n sda-prod exec svc/postgres-cluster-ro -c postgres -- psql -U postgres  -d sda -c "
+extra_options=""
+
+if [ "$no_header" = true ]; then
+    extra_options="-tA"
+else
+    extra_options="-t"
+fi
+
+# Query the files table for the specified filepath
+kubectl -n sda-prod exec svc/postgres-cluster-ro -c postgres -- psql $extra_options -U postgres  -d sda -c "
 SELECT * FROM sda.files
 WHERE $filepath_condition 
 "
