da2f7dcc20db42753dd24a41102e372a8d8a8037
Nanjiang Shu <nanjiang.shu@gmail.com>
2025-06-27 00:19:19 +0200

    add script

---
+++ scripts/get_stableid_filepath_user_list.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script get stable_id and filepath (without username) that will be provided to the submitter for a given dataset folder.
+
+dataset_folder=$1
+if [ "$dataset_folder" == "" ];then
+    echo "Usage: $0 <dataset_folder>"
+    exit 1
+fi
+
+kubectl -n sda-prod exec svc/postgres-cluster-ro -c postgres -- psql -U postgres -tA -d sda -c "
+SELECT stable_id, submission_file_path, submission_user
+FROM sda.files
+WHERE submission_file_path LIKE '%$dataset_folder/%'
+  AND stable_id IS NOT NULL
+  AND submission_file_path IS NOT NULL
+  AND submission_user IS NOT NULL
+" | awk 'NF' | sort -u | tr '|' '\t'
