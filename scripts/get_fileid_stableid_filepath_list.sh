4cdc93fd22d8036cd2bfa7bdf3d014e9e774f9b9
Nanjiang Shu <nanjiang.shu@gmail.com>
2025-06-27 00:06:25 +0200

    add script

--- scripts/get_fileid_stableid_filepath_list.sh
+++ scripts/get_fileid_stableid_filepath_list.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# This script get stable_id and filepath (without username) that will be provided to the submitter for a given dataset folder.
 
 dataset_folder=$1
 if [ "$dataset_folder" == "" ];then
@@ -8,6 +7,6 @@ if [ "$dataset_folder" == "" ];then
 fi
 
 kubectl -n sda-prod exec svc/postgres-cluster-ro -c postgres -- psql -U postgres -tA -d sda -c "
-SELECT stable_id, submission_file_path FROM sda.files
+SELECT id, stable_id, submission_file_path FROM sda.files 
 WHERE submission_file_path LIKE '%$dataset_folder/%'
-" | awk 'NF' | sort -u |  tr '|' '\t'
+" | awk 'NF' | sort -u |  tr '|' '\t' 
