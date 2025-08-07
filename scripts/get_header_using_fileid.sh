7b66e68256220e27d7c60f7facc0fb6a915ef9dd
Nanjiang Shu <nanjiang.shu@gmail.com>
2025-08-07 15:05:41 +0200

    update script

--- scripts/get_header_using_fileid.sh
+++ scripts/get_header_using_fileid.sh
@@ -28,5 +28,5 @@ fi
 # Query the files table for the specified fileid
 kubectl -n sda-prod exec svc/postgres-cluster-ro -c postgres -- psql -tA -U postgres  -d sda -c "
 SELECT header FROM sda.files
-WHERE fileid = '$fileid'
+WHERE id = '$fileid'
 "
