422abb8772cd8df7d38253367140637c84a14ac2
Nanjiang Shu <nanjiang.shu@gmail.com>
2025-10-30 20:38:20 +0100

    not using admin api for getting ingest status

--- scripts/get_ingest_status.sh
+++ scripts/get_ingest_status.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
+# this script checks the file status statistics
 
 set -euo pipefail
 
-# this script checks the file status statistics during ingestion
+SCRIPT_DIR=$(dirname "$0")
+binpath=$(realpath "$SCRIPT_DIR")
 
 usage="""
-Usage: $0 <user> <dataset_folder>
+Usage: $0 <user> <dataset_folder> <outdir>
+<user> : the LSAAI user ID
+<dataset_folder> : the dataset folder name
+<outdir> : directory to save intermediate and final results
 """
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
   echo "$usage"
   exit 1
 fi
 
 user="$1"
 dataset_folder="$2"
+outdir="$3"
 
-sda-admin file list --user "$user" | jq -r --arg ds "$dataset_folder" '.[] | select((.inboxPath | contains($ds))) | .fileStatus' | sort | uniq -c  
+if [ ! -d "$outdir" ]; then
+  mkdir -p "$outdir"
+fi
+
+bash $binpath/query_userfiles.sh $user $dataset_folder > $outdir/$dataset_folder.userfiles.txt
+
+awk -F\| '{print $1}'  $outdir/$dataset_folder.userfiles.txt  | sort -u > $outdir/$dataset_folder.fileidlist.txt
+
+bash $binpath/query_status_in_fileeventlog_with_fileidlist.sh $outdir/$dataset_folder.fileidlist.txt  > $outdir/$dataset_folder.status.list.txt
+
+awk -F\| '{print $2}' $outdir/$dataset_folder.status.list.txt | awk -F, '{print $1}' | sort | uniq -c
