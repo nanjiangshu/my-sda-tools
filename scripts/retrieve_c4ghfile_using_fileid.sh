fe4361f97f5cc8260b894360a4496ba92ae5bdcb
Nanjiang Shu <nanjiang.shu@gmail.com>
2026-01-09 11:41:55 +0100

    update script for retriving archived file

--- scripts/retrieve_c4ghfile_using_fileid.sh
+++ scripts/retrieve_c4ghfile_using_fileid.sh
@@ -48,7 +48,7 @@ RetrieveFile(){
     cp "$tmpdir/$fileid.c4gh" "$outdir/$fileid.bak.c4gh"
     
     # Decrypting
-    crypt4gh decrypt -s "$keyfile" < "$tmpdir/$fileid.c4gh" > "$tmpdir/$fileid.decrypted"
+    crypt4gh decrypt -s "$keyfile" -f "$tmpdir/$fileid.c4gh" -o "$tmpdir/$fileid.decrypted"
 
     cp "$tmpdir/$fileid.decrypted" "$outdir/$fileid"
     echo "File retrieved and decrypted successfully: $fileid"
