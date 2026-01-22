fd67352da8e2eff3cc29de64f456d193c5eacc68
Nanjiang Shu <nanjiang.shu@gmail.com>
2026-01-22 13:53:31 +0100

    remove trailing white space

--- scripts/get_file_stableids_from_datasetid.sh
+++ scripts/get_file_stableids_from_datasetid.sh
@@ -6,6 +6,7 @@ if [ "$dataset_stableID" == "" ];then
 fi
 
 
+# ensure the output has no trailing white spaces at the beginning or end of each line
 kubectl -n sda-prod exec svc/postgres-cluster-ro -c postgres -- psql -U postgres -t -d sda -c "
 SELECT stable_id FROM sda.files WHERE id IN (SELECT file_id FROM sda.file_dataset WHERE dataset_id = (SELECT id FROM sda.datasets WHERE stable_id = '$dataset_stableID'))
-" | awk 'NF' | sort -u 
+" | awk 'NF' | sort -u | sed 's/^[[:space:]]\+//;s/[[:space:]]\+$//'
