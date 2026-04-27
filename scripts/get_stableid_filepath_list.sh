67677f5fbb0f2261a41e73c99be35bcbaa6ca421
nanjiangshu <nanjiang.shu@gmail.com>
2026-04-27 22:26:28 +0200

    update script

--- scripts/get_stableid_filepath_list.sh
+++ scripts/get_stableid_filepath_list.sh
@@ -54,7 +54,7 @@ else
   kubectl -n sda-prod exec svc/postgres-cluster-ro -c postgres -- psql -U postgres -tA -d sda -c "
   SELECT stable_id, submission_file_path FROM sda.files
   WHERE submission_file_path LIKE '%$dataset_folder/%'
-  # AND stable_id IS NOT NULL
-  # AND submission_file_path IS NOT NULL
+  AND stable_id IS NOT NULL
+  AND submission_file_path IS NOT NULL
   " | awk 'NF' | sort -u |  tr '|' '\t'
 fi
