8a0005990c6673064aad52bc23745e79fd1a8abe
nanjiangshu <nanjiang.shu@gmail.com>
2026-05-26 15:19:56 +0200

    update script for checking db version

--- scripts/check_dbversion_fega.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# check db version for FEGA
-
-kubectl -n fega-staging exec svc/fega-staging-sda-postgres-ro -c postgres -- psql -tA -U postgres -d sda -c "
-SELECT version, description FROM sda.dbschema_version ORDER BY version DESC LIMIT 1;
-" 
