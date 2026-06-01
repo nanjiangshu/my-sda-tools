2ab4c2f77c26e00bac112c4be8bd2ba2d8d02bf3
nanjiangshu <nanjiang.shu@gmail.com>
2026-06-01 16:09:27 +0200

    expand script for bot bp and fega

--- scripts/query_fileeventlog_with_fileid.sh
+++ scripts/query_fileeventlog_with_fileid.sh
@@ -1,17 +1,61 @@
 #!/bin/bash
 
 # This script queries the file_event_log table in the sda database for a given file ID.
+target="bp-prod"
+
+usage="Usage: $0 [-target <target>] <file_id>
+OPTIONS:
+    -target <target>    Specify the target environment (default: bp-prod), can be one of:
+                        fega-staging, fega-prod, bp-staging, bp-prod
+"
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -target)
+            target="$2"
+            shift 2
+            ;;
+        *)
+            file_id="$1"
+            shift
+            ;;
+    esac
+done
+
+case $target in
+    fega-staging)
+        namespace="fega-staging"
+        service="svc/fega-staging-sda-postgres-ro"
+        ;;
+    fega-prod)
+        namespace="fega-prod"
+        service="svc/fega-prod-sda-postgres-ro"
+        ;;
+    bp-staging)
+        namespace="sda-staging"
+        service="svc/cnpg-sda-staging-ro"
+        ;;
+    bp-prod)
+        namespace="sda-prod"
+        service="svc/postgres-cluster-ro"
+        ;;
+    *)
+        echo "Unknown target environment: $target"
+        echo "$usage"
+        exit 1
+esac
 
 # Check if a file_id was provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <file_id>"
+if [ -z "$file_id" ]; then
+    echo "$usage"
     exit 1
 fi
 
 # Remove leading/trailing whitespaces
-file_id=$(echo "$1" | xargs)
+file_id=$(echo "$file_id" | xargs)
 
-kubectl -n sda-prod exec svc/postgres-cluster-ro -c postgres -- psql -U postgres -t -d sda -c "
+kubectl -n $namespace exec $service -c postgres -- psql -U postgres -t -d sda -c "
 SELECT * FROM sda.file_event_log
 WHERE file_id = '$file_id'
 ORDER BY started_at DESC
