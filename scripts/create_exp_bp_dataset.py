be37afbe90746cf1388d79997ef052ff86a61ed3
nanjiangshu <nanjiang.shu@gmail.com>
2026-08-28 11:54:01 +0200

    update script

--- scripts/create_exp_bp_dataset.py
+++ scripts/create_exp_bp_dataset.py
@@ -145,7 +145,7 @@ def create_xml_files(metadata_path, identifier, images_data, annotation_info):
 			</STRING_ATTRIBUTE>
 			<STRING_ATTRIBUTE>
 				<TAG>type_of_dataset</TAG>
-				<VALUE>Non-Clinical / Cryptonimized</VALUE>
+                <VALUE>Clinical / Anonymized</VALUE>
 			</STRING_ATTRIBUTE>
 			<STRING_ATTRIBUTE>
 				<TAG>terms_of_use_version</TAG>
@@ -451,7 +451,7 @@ def create_private_files(private_path, identifier, create_datacite=True):
     rems_xml = f"""<?xml version="1.0" encoding="UTF-8"?>
 <REMS_SET xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
     <REMS alias="rems_{identifier}">
-        <WORKFLOW_ID>1</WORKFLOW_ID>
+        <WORKFLOW_ID>69</WORKFLOW_ID>
         <ORGANISATION_ID>demo</ORGANISATION_ID>
         <DATASET_REF alias="{identifier}"/>
         <ATTRIBUTES>
