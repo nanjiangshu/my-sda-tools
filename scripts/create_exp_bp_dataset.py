1f0400aa90a527c16ecac692d2b0e3015e1d32f4
nanjiangshu <nanjiang.shu@gmail.com>
2026-08-28 11:32:06 +0200

    update script

--- scripts/create_exp_bp_dataset.py
+++ scripts/create_exp_bp_dataset.py
@@ -452,7 +452,7 @@ def create_private_files(private_path, identifier, create_datacite=True):
 <REMS_SET xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
     <REMS alias="rems_{identifier}">
         <WORKFLOW_ID>1</WORKFLOW_ID>
-        <ORGANISATION_ID>Demo org</ORGANISATION_ID>
+        <ORGANISATION_ID>demo</ORGANISATION_ID>
         <DATASET_REF alias="{identifier}"/>
         <ATTRIBUTES>
             <STRING_ATTRIBUTE>
